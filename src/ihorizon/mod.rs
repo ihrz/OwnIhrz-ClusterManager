5a0ed7b9457b882fbf3e10e55d435d018ae53b73
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2024-01-13 21:37:26 +0100

    e
    Co-Authored-By: Pouplos <56542176+WyeneCloud@users.noreply.github.com>

--- src/ihorizon/mod.rs
+++ src/ihorizon/mod.rs
@@ -1,7 +1,11 @@
-use serde::Deserialize;
-use rocket::data::{self, Data::Outcome::*, FromData, ToByteUnit};
-use rocket::request::Request;
+use rocket::data::{
+    self,
+    Outcome::{Failure, Success},
+    FromData, ToByteUnit,
+};
 use rocket::http::Status;
+use rocket::request::Request;
+use serde::Deserialize;
 
 #[derive(FromForm, Deserialize)]
 pub struct Bot<'r> {
@@ -22,13 +26,49 @@ pub struct CustomIhorizon<'r> {
 }
 
 #[rocket::async_trait]
+impl<'r> FromData<'r> for Bot<'r> {
+    type Error = String;
+
+    async fn from_data(req: &'r Request<'_>, data: Data<'r>) -> data::Outcome<'r, Self> {
+        match serde_urlencoded::from_reader::<Bot<'r>, _>(data.open(10.megabytes())) {
+            Ok(custom) => data::Outcome::Success(custom),
+            Err(_) => {
+                data::Outcome::Failure((Status::BadRequest, "Invalid data format".to_string()))
+            }
+        }
+    }
+}
+
+
+
+impl<'r> CustomIhorizon<'r> {
+    pub fn new() -> Self {
+        Self {
+            auth: "",
+            owner_one: "",
+            owner_two: None,
+            expire_in: 0,
+            bot: Bot {
+                id: "",
+                username: "",
+                public: false,
+            },
+            admin_key: "",
+            code: "",
+        }
+    }
+}
+
+#[rocket::async_trait]
 impl<'r> FromData<'r> for CustomIhorizon<'r> {
     type Error = String;
 
     async fn from_data(req: &'r Request<'_>, data: Data<'r>) -> data::Outcome<'r, Self> {
         match serde_urlencoded::from_reader::<CustomIhorizon<'r>, _>(data.open(10.megabytes())) {
             Ok(custom) => data::Outcome::Success(custom),
-            Err(_) => data::Outcome::Failure((Status::BadRequest, "Invalid data format".to_string())),
+            Err(_) => {
+                data::Outcome::Failure((Status::BadRequest, "Invalid data format".to_string()))
+            }
         }
     }
 }
