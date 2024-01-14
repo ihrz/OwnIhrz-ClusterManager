2c9863a5fd39c022bb578cc1f749218390e3098e
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2024-01-14 19:02:58 +0100

    Okayy

--- structure/config_data.go
+++ structure/config_data.go
@@ -5,6 +5,12 @@ type APIConfig struct {
 	ClientID string `mapstructure:"clientID"`
 }
 
+type Cluster struct {
+	name       string `mapstructure:"name"`
+	MaxCluster int    `mapstructure:"max_container"`
+}
+
 type Config struct {
-	API APIConfig `mapstructure:"api"`
+	API     APIConfig `mapstructure:"api"`
+	Cluster Cluster   `mapstructure:"cluster"`
 }
