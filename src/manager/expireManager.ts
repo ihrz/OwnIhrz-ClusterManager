94f6feb3d7f36162cb924f63da988b44260a3a76
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2024-05-05 17:02:05 +0000

    Update expireManager.ts

--- src/manager/expireManager.ts
+++ src/manager/expireManager.ts
@@ -11,32 +11,22 @@ async function Refresh() {
 
     let now = new Date().getTime();
 
-    result.forEach((v) => {
-        let userId = v.id;
-        let botId = v.value;
+    for (let i = 0; i < result.length; i++) {
+        let data = result[i].value;
+        let userId = result[i].id;
 
-        console.log(userId, botId)
-    });
+        for (let code in data) {
+            let bot = data[code];
 
-    // for (let i in result) {
-    //     for (let c in result[i]) {
-    //         if (!result[i][c].Code || result[i][c].PowerOff) continue;
-    //         if (now >= result[i][c].ExpireIn) {
-    //             await db.set(`OWNIHRZ.${i}.${c}.PowerOff`, true);
+            if (!bot.Code || bot.PowerOff) continue;
+            if (now >= bot.ExpireIn) {
+                await db.set(`OWNIHRZ.${userId}.${code}.PowerOff`, true);
 
-    //             [
-    //                 {
-    //                     line: `pm2 stop ${result[i][c].Code} -f`,
-    //                     cwd: process.cwd()
-    //                 },
-    //                 {
-    //                     line: `pm2 delete ${result[i][c].Code}`,
-    //                     cwd: process.cwd()
-    //                 },
-    //             ].forEach((index) => { execSync(index.line, { stdio: [0, 1, 2], cwd: index.cwd }); });
-    //         }
-    //     }
-    // }
+                execSync(`pm2 stop ${code} -f`, { stdio: [0, 1, 2], cwd: process.cwd() });
+                execSync(`pm2 delete ${code}`, { stdio: [0, 1, 2], cwd: process.cwd() });
+            }
+        }
+    }
 
     return;
 };
